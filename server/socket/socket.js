bef078851273e655c64047d51c8e1c241ce7b47c
Ashutosh <ashutoshash.az22@gmail.com>
2024-11-23 22:57:58 +0530

    backend fixes

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -1,8 +1,18 @@
-// src/socket.js
-const { Server } = require("socket.io");
+// const { Server } = require("socket.io");
+
+const socketIo = require("socket.io");
 
 const initializeSocket = (server) => {
-  const io = new Server(server, {
+  // const io = new Server(server, {
+  //   cors: {
+  //     origin: ["http://localhost:5000", "https://vibe-rosy.vercel.app"],
+  //     methods: ["GET", "POST"],
+  //     allowedHeaders: ["Content-Type", "Authorization"],
+  //     credentials: true,
+  //   },
+  // });
+
+  const io = socketIo(server, {
     cors: {
       origin: ["http://localhost:5000", "https://vibe-rosy.vercel.app"],
       methods: ["GET", "POST"],
